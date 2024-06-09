65db768ac6a3d4dcf8eb668b2331e4cf30ef1c60
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-06-09 16:21:00 +0200

    added utils.ts file and added properties

--- index.ts
+++ index.ts
@@ -2,9 +2,18 @@
 // Write a function that will only accept numbers and attend to 
 // all TypeScript weakness flags.
 // : number
-const reviewTotalDisplay = document.querySelector('#reviews')
 
-const reviews = [
+import { showReviewTotal, populateUser } from './utils'
+
+let isOpen : boolean 
+
+// Reviews
+const reviews : {
+    name : string;
+    stars : number;
+    loyaltyUser : boolean;
+    date : string;
+}[] = [
     {
         name: 'Sheia',
         stars: 5,
@@ -23,4 +32,45 @@ const reviews = [
         loyaltyUser: true,
         date: '27-03-2021'
     },
+    
 ]
+// User
+const you: {
+    firstName: string;
+    lastName: string;
+    isReturning: boolean;
+    age: number;
+    stayedAt: string[];
+} = {
+    firstName: 'Bobby',
+    lastName: 'Brown',
+    isReturning: true,
+    age: 35,
+    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+}
+
+//Properties
+const properties : {
+    image: string;
+    title: string;
+    price: number;
+    location: {
+        firstLine: string;
+        city: string;
+        code: number;
+        country: string;
+    };
+    contact: string;
+    isAvailabe: boolean;
+}[] = [
+    {
+        image: string;
+        
+    }
+]
+
+// Functions
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+
+populateUser(you.isReturning, you.firstName)
