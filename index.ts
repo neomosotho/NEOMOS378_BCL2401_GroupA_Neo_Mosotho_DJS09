166e920d4dd0b19d41a27385b584acdd9ab767c2
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-06-09 16:47:09 +0200

    added images and properties class

--- index.ts
+++ index.ts
@@ -64,8 +64,47 @@ const properties : {
     isAvailabe: boolean;
 }[] = [
     {
-        image: string;
+        image: '',
+        title: 'Colombian Shack',
+        price: 45,
+        location: {
+            firstLine: 'Shack 37',
+            city: 'Bogota',
+            code: 45632,
+            country: 'Colombia'
+        },
+        contact: 'marywinkle@gmail.com',
+        isAvailable: true
         
+    },
+    {
+        image: '',
+        title: 'Polish Cottage',
+        price: 34,
+        location: {
+            firstLine: 'no 23',
+            city: 'Gdansk',
+            code: 343903,
+            country: 'Poland'
+        },
+        contact: 'garydavis@hotmail.com',
+        isAvailable: false
+
+    },
+
+    {
+        image: '',
+        title: 'London Flat',
+        price: 23,
+        location: {
+            firstLine: 'flat 15',
+            city: 'London',
+            code: 35433,
+            country: 'United Kingdom'
+        },
+        contact: 'andyluger@aol.com',
+        isAvailable: true
+
     }
 ]
 
