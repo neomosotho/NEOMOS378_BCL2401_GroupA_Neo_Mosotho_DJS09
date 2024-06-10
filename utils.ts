44893980bba6c60f4d845bdd5fd5ee59d1e5863a
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-06-10 14:51:18 +0200

    "Refactored codebase: updated imports, enums, and types; removed scripts.js; modified utils.ts and webpack.config.js; updated package.json"

--- utils.ts
+++ utils.ts
@@ -1,10 +1,12 @@
 const reviewTotalDisplay = document.querySelector('#reviews')
 const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
+import { LoyaltyUser, Permissions } from './enums'
 
-export function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
-    const iconDisplay = isLoyalty ? '⭐' : ''
-    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) {
+    const iconDisplay = LoyaltyUser.GOLD_USER ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + 
+    reviewer + ' ' + iconDisplay
 }
 
 export function populateUser(isReturning : boolean, userName: string ) {
@@ -13,3 +15,33 @@ export function populateUser(isReturning : boolean, userName: string ) {
     }
     userNameDisplay.innerHTML = userName
 }
+
+export function showDetails(value: boolean | Permissions, element : HTMLDivElement, price: number) {
+    if (value) {
+        const priceDisplay = document.createElement('div')
+        priceDisplay.innerHTML = price.toString() + '/night'
+        element.appendChild(priceDisplay)
+    }
+}
+
+export function makeMultiple(value: number) {
+    if (value > 1 || value == 0 ) {
+        return 's'
+    } else return ''
+}
+
+// function add(firstValue, secondValue) {
+//     let result
+//     if (typeof firstValue === 'number' && typeof secondValue === 'number') {
+//         result = firstValue + secondValue
+//     }
+//     if (typeof firstValue === 'string' && typeof secondValue === 'string') {
+//         result = firstValue + ' ' + secondValue
+//     }
+//     if (typeof firstValue === 'number' && typeof secondValue === 'string') {
+//         console.log('cannot perform this addition')
+//     }
+//     if (typeof firstValue === 'string' && typeof secondValue === 'number') {
+//         console.log('cannot perform this addition')
+//     }
+// }
